9d3d1bc3a4d243d1371a1f5b60167dbd77c6c8a6
2608abigailmorais <85850400+2608abigailmorais@users.noreply.github.com>
2022-05-17 17:11:03 -0300

    Add files via upload

---
+++ conf/conf.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Banco de Dados
+    define('HOST', 'localhost');  
+    define('DBNAME', 'rec');    
+    define('USER', 'root');  
+    define('PASSWORD','');
+
+
+    
+    define('DRIVER', 'mysql'); 
+    define('CHARSET', 'utf8');
+
+    // Geral da Aplicação
+    define('NOME_DO_PROJETO','Recuperação');
+    define('DESCRICAO_DO_PROJETO','Um Pequeno exemplo usando PDO');
+?>
